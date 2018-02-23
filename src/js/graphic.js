eb1847f2292ba1168a00ac42ee6fc97499121ffd
russellgoldenberg <russellgoldenberg@gmail.com>
2018-02-23 17:28:22 -0500

    maps

--- src/js/graphic.js
+++ src/js/graphic.js
@@ -1,4 +1,8 @@
 // D3 is included by globally by default
+import * as topojson from 'topojson';
+
+const TEST = true
+
 const $map = d3.select('#map');
 const $svg = $map.select('svg');
 const $g = $svg.select('g');
@@ -8,7 +12,23 @@ let width = 0;
 let height = 0;
 let mobile = false;
 
-function resize() {
+let world = null;
+
+function setup() {
+	const projection = d3
+		.geoMercator()
+		.scale(width / 2 / Math.PI)
+		.translate([width / 2, height / 2]);
+
+	const path = d3.geoPath().projection(projection);
+	const prop = TEST ? 'countries' :'nat_earth_geojson'
+	const json = world.objects[prop];
+	const feature = topojson.feature(world, json);
+	console.log(feature)
+	$svg.append('path').at('d', path(feature));
+}
+
+function updateDimensions() {
 	width = $map.node().offsetWidth;
 
 	mobile = width < BP;
@@ -16,11 +36,23 @@ function resize() {
 	// const h = w * ratio
 
 	height = window.innerHeight * 0.67;
+}
+
+function resize() {
+	updateDimensions();
 	$svg.at({ width, height });
 }
 
 function init() {
-	resize();
+	const path = 'assets/data';
+	const file = TEST ? 'world-110m' : 'nat_earth_topo'
+	d3.loadData(`${path}/${file}.json`, (err, results) => {
+		if (err) console.error(err);
+		world = results[0];
+		updateDimensions();
+		setup();
+		resize();
+	});
 }
 
 export default { init, resize };
