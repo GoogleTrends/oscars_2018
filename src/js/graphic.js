c66880e54cf23e2b00bffa36eba4f2bc06ba0beb
russellgoldenberg <russellgoldenberg@gmail.com>
2018-02-23 16:32:03 -0500

    map

--- src/js/graphic.js
+++ src/js/graphic.js
@@ -1,9 +1,26 @@
 // D3 is included by globally by default
+const $map = d3.select('#map');
+const $svg = $map.select('svg');
+const $g = $svg.select('g');
 
-function resize() {}
+const BP = 800;
+let width = 0;
+let height = 0;
+let mobile = false;
+
+function resize() {
+	width = $map.node().offsetWidth;
+
+	mobile = width < BP;
+	// const ratio = 1
+	// const h = w * ratio
+
+	height = window.innerHeight * 0.67;
+	$svg.at({ width, height });
+}
 
 function init() {
-	console.log('Make something awesome!');
+	resize();
 }
 
 export default { init, resize };
