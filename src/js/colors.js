35afc5f35f02283a411981d74019ab6b9a2b4af1
russellgoldenberg <russellgoldenberg@gmail.com>
2018-02-26 13:48:33 -0500

    update dev version

---
+++ src/js/colors.js
@@ -0,0 +1,13 @@
+export default {
+	default: '#efefef',
+	categorical: [
+		'#F67088',
+		'#CE8F31',
+		'#96A331',
+		'#32B165',
+		'#35ACA4',
+		'#38A7D0',
+		'#A38CF4',
+		'#F461DD'
+	]
+};
--- src/js/graphic.js
+++ src/js/graphic.js
@@ -1,10 +1,13 @@
 // D3 is included by globally by default
 import * as topojson from 'topojson';
 import { geoRobinson } from 'd3-geo-projection';
+import colors from './colors';
+import months from './months';
 
 const TEST = false;
 
 const $map = d3.select('#map');
+const $uiLabel = $map.select('.ui__label');
 const $svg = $map.select('svg');
 const $g = $svg.select('g');
 
@@ -12,7 +15,53 @@ const BP = 800;
 let width = 0;
 let height = 0;
 let mobile = false;
-let world = null;
+let worldData = null;
+let movieYearData = null;
+let movieMonthData = null;
+let currentMonth = 1;
+const movieColors = {};
+
+function getMovieColor({ properties }) {
+	const { name } = properties;
+	const country = movieMonthData.find(d => d.key === name);
+	if (!country) return colors.default;
+
+	const m = country.values.find(d => d.month === currentMonth);
+	return movieColors[m.country_max];
+}
+
+function update() {
+	$g.selectAll('.region').st('fill', getMovieColor);
+}
+
+function handleSlider() {
+	currentMonth = +this.value;
+	const m = months[currentMonth - 1];
+	$uiLabel.text(`${m} 2017`);
+	update();
+}
+
+function createKey() {
+	const nested = d3
+		.nest()
+		.key(d => d.max)
+		.rollup(v => v.length)
+		.entries(movieYearData);
+
+	nested.sort((a, b) => d3.descending(a.value, b.value));
+
+	nested.forEach((d, i) => (movieColors[d.key] = colors.categorical[i]));
+
+	const movieColorsArr = Object.keys(movieColors);
+	$map
+		.select('.map__key ul')
+		.selectAll('li')
+		.data(movieColorsArr)
+		.enter()
+		.append('li')
+		.text(d => d)
+		.st('color', d => movieColors[d]);
+}
 
 function setup() {
 	const projection = geoRobinson()
@@ -21,16 +70,19 @@ function setup() {
 
 	const path = d3.geoPath().projection(projection);
 	const prop = TEST ? 'countries' : 'countries_geo_github';
-	const json = world.objects[prop];
-	const feature = topojson.feature(world, json);
-	console.log({ json, feature });
+	const json = worldData.objects[prop];
+	const feature = topojson.feature(worldData, json);
 
 	$g
-		.selectAll('path')
-		.data(feature.features)
+		.selectAll('.region')
+		.data(feature.features, d => d.id)
 		.enter()
 		.append('path')
-		.at('d', path);
+		.at('d', path)
+		.at('class', d => `region ${d.id}`);
+
+	createKey();
+	$map.select('.slider').on('input', handleSlider);
 }
 
 function updateDimensions() {
@@ -48,16 +100,36 @@ function resize() {
 	$svg.at({ width, height });
 }
 
+function cleanMovieMonthData(data) {
+	const clean = data.map(d => ({
+		...d,
+		month: +d.month
+	}));
+
+	return d3
+		.nest()
+		.key(d => d.country)
+		.entries(clean);
+}
+
 function init() {
 	const path = 'assets/data';
-	const file = TEST ? 'world-110m' : 'github_geototopo_v1';
+	const geo = TEST ? 'world-110m' : 'github_geototopo_v1';
 	updateDimensions();
 
-	d3.loadData(`${path}/${file}.json`, (err, response) => {
-		world = response[0];
-		setup();
-		resize();
-	});
+	d3.loadData(
+		`${path}/${geo}.json`,
+		`${path}/movies_by_year.csv`,
+		`${path}/movies_by_month.csv`,
+		(err, response) => {
+			worldData = response[0];
+			movieYearData = response[1];
+			movieMonthData = cleanMovieMonthData(response[2]);
+			setup();
+			resize();
+			update();
+		}
+	);
 }
 
 export default { init, resize };
